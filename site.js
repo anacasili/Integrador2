3d0d9e6cf3a131b33c76502b273c4245e44a6aa0
anacasili <128873798+anacasili@users.noreply.github.com>
2024-01-04 16:35:48 -0300

    Add files via upload

---
+++ site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//atendimentos
+document.querySelector('.btn-custom').addEventListener('click', function(event) {
+  event.preventDefault();
+  alert('Redirecionando para WhatsApp...');
+  window.location.href = 'https://web.whatsapp.com';
+});
+
